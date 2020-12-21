162aef647dd1d9f40fd39bf28352f419ef431cbf
Sebastian Montoya <seb246@gmail.com>
2020-12-20 22:11:54 -0500

    added find method to find cats in database

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const moongoose = require(`mongoose`);
+const mongoose = require(`mongoose`);
 
 mongoose 
     .connect(`mongodb://localhost/exampleApp`, {
         useCreateIndex: true,
         useNewUrlParser: true,
         useUnifiedTopology: true
-    });
-    .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`));
+    })
+    .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
     .catch(err => console.error(`error connecting to mongo`, err));
 
 const Cat = mongoose.model(`Cat`, {name: String});
@@ -14,5 +14,11 @@ const kitty = new Cat({name: `Ironhacker`});
 
 kitty
     .save()
-    .then(newCat => console.log(`A new cat is created: ${newCat}!`));
+    .then(newCat => console.log(`A new cat is created: ${newCat}!`))
     .catch(err => console.log(`Error while creating new car ${err}`));
+
+Cat.find()
+    .then(catsFromDB => {
+        catsFromDB.forEach(oneCat => console.log(`one cat -->: ${oneCat.name}`));
+    })
+    .catch(error => console.log(`Error when getting cats: ${error}`));
